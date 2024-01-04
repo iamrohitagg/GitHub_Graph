506fe50c031d06435626555161cea65f7cd76495
Rohit Aggarwal <rohitaggarwal@Rohits-MacBook-Air.local>
2024-01-04 10:59:43 +0530

    fix: added check for wrong repo

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const jsonfile = require("jsonfile");
-
-const moment = require("moment");
-const simpleGit = require("simple-git");
-
-const random = require("random");
 const FILE_PATH = "./data.json";
+const simpleGit = require("simple-git");
+const jsonfile = require("jsonfile");
+const moment = require("moment");
+const random = require("random");
 
-
-// const git: SimpleGit = simpleGit('https://github.com/iamrohitagg/GitHub_Graph.git');
-
+// Change the working directory to where your local repository is located
+const git = simpleGit("/Users/rohitaggarwal/Desktop/test/GitHub_Graph");
 
 const makeCommit = (n) => {
-  if (n === 0) return simpleGit().push();
+  if (n === 0) {
+    // Push changes to the remote repository
+    git.push(["-u", "origin", "master"], (err, result) => {
+      if (err) {
+        console.error("Error pushing to remote:", err);
+      } else {
+        console.log("Pushed changes to remote repository");
+      }
+    });
+    return;
+  }
+
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
@@ -20,15 +28,24 @@ const makeCommit = (n) => {
     .add(x, "w")
     .add(y, "d")
     .format();
+
   const data = {
     date: DATE,
   };
   console.log(DATE);
+
   jsonfile.writeFile(FILE_PATH, data, () => {
-    simpleGit()
+    git
       .add([FILE_PATH])
-      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n))
-      .push();
+      .commit(DATE, { "--date": DATE })
+      .push(["-u", "origin", "origin"], (err, result) => {
+        if (err) {
+          console.error("Error pushing to remote:", err);
+        } else {
+          console.log("Pushed changes to remote repository");
+          makeCommit(--n);
+        }
+      });
   });
 };
 
